f10c19d72384336ab9458f3256a4fbef0c1ffe68
John H. Nguyen <buffbuddha@gmail.com>
2022-06-20 00:58:36 -0700

    Revert "Log to see if the env vars are being set."
    This reverts commit 7ac72298aecbc14d7db74a849aae55fee49c05fe.

--- app.js
+++ app.js
@@ -54,8 +54,6 @@ const bot = new Discord.Client({ intents: [Discord.Intents.FLAGS.GUILDS, Discord
 
 bot.login(process.env.token)
 const rest = new REST({ version: '9' }).setToken(process.env.token);
-logger.info('@@@redis host:')
-logger.log(process.env.redis_host)
 
 bot.on('ready', (evt) => {
   logger.info('Connected')
@@ -133,11 +131,11 @@ bot.on('interactionCreate', async interaction => {
         await playerSheetInteraction.handleGet(interaction)
       }
     } else if (commandName === 'm' || commandName === 't') {
-      const subCmd = options.getSubcommand()
-      await poolInteraction.buildPrompt(interaction, commandName, subCmd)
+        const subCmd = options.getSubcommand()
+        await poolInteraction.buildPrompt(interaction, commandName, subCmd)
     }
   } catch (error) {
-    await interaction.reply({
+    await interaction. reply({
       content: error.toString()
     })
   }
