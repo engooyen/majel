3379d299cf0fcecb037a3a18d1f946cd817b7bbf
John H. Nguyen <buffbuddha@gmail.com>
2022-09-26 13:02:07 -0700

    Added 6 as an option for crit

--- commands/commands.js
+++ commands/commands.js
@@ -88,7 +88,8 @@ commands.push(new SlashCommandBuilder()
                 { name: '2', value: 2 },
                 { name: '3', value: 3 },
                 { name: '4', value: 4 },
-                { name: '5', value: 5 }
+                { name: '5', value: 5 },
+                { name: '6', value: 6 }
             )
     )
     .addIntegerOption(option =>
